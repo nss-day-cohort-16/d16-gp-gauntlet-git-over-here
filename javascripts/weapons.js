108f22e595afd6a5be2b99f79eed97d72297e40b
Samuel Phillips <samphillips1879@gmail.com>
2016-11-14 11:22:49 -0600

    Sam attack sounds

--- javascripts/weapons.js
+++ javascripts/weapons.js
@@ -10,6 +10,7 @@ var Gauntlet = (function(oldGauntlet) {
     this.damage = 1;
     this.hands = 2;
     this.image = "images/weapons/hand.jpg";
+    this.sound = "sound/weapons/hands.mp3"
 
     this.toString = function() {
       return this.name;
@@ -21,6 +22,7 @@ var Gauntlet = (function(oldGauntlet) {
     this.damage = 4;
     this.hands = 1;
     this.image = "images/weapons/dagger.png";
+    this.sound = "sound/weapons/dagger.mp3";
   };
   oldGauntlet.Weapons.Dagger.prototype = new oldGauntlet.Weapons.Weapon();
 
@@ -29,6 +31,7 @@ var Gauntlet = (function(oldGauntlet) {
     this.damage = 14;
     this.hands = 2;
     this.image = "images/weapons/broadsword.jpg";
+    this.sound = "sound/weapons/broadsword.mp3";
   };
   oldGauntlet.Weapons.BroadSword.prototype = new oldGauntlet.Weapons.Weapon();
 
@@ -37,6 +40,7 @@ var Gauntlet = (function(oldGauntlet) {
     this.damage = 18;
     this.hands = 2;
     this.image = "images/weapons/waraxe.jpg";
+    this.sound = "sound/weapons/waraxe.mp3";
   };
   oldGauntlet.Weapons.WarAxe.prototype = new oldGauntlet.Weapons.Weapon();
 
@@ -45,6 +49,7 @@ var Gauntlet = (function(oldGauntlet) {
     this.damage = 2;
     this.hands = 1;
     this.image = "images/weapons/fryingpan.jpg";
+    this.sound = "sound/weapons/fryingpan.mp3";
   };
   oldGauntlet.Weapons.FryingPan.prototype = new oldGauntlet.Weapons.Weapon();
 
@@ -53,6 +58,7 @@ var Gauntlet = (function(oldGauntlet) {
     this.damage = 15;
     this.hands = 2;
     this.image = "images/weapons/machinegun.png";
+    this.sound = "sound/weapons/machinegun.mp3";
   };
   oldGauntlet.Weapons.MachineGun.prototype = new oldGauntlet.Weapons.Weapon();
 
@@ -61,6 +67,7 @@ var Gauntlet = (function(oldGauntlet) {
     this.damage = 30;
     this.hands = 2;
     this.image = "images/weapons/truth.png";
+    this.sound = "sound/weapons/truthbomb.mp3";
   };
   oldGauntlet.Weapons.TruthBomb.prototype = new oldGauntlet.Weapons.Weapon();
 
@@ -69,6 +76,7 @@ var Gauntlet = (function(oldGauntlet) {
    this.damage = 39;
    this.hands = 1;
    this.image = "images/weapons/fecal.jpg";
+   this.sound = "sound/weapons/fecalfedora.mp3";
   };
   oldGauntlet.Weapons.FecalFedora.prototype = new oldGauntlet.Weapons.Weapon();
 
@@ -77,6 +85,7 @@ var Gauntlet = (function(oldGauntlet) {
    this.damage = 12;
    this.hands = 3;
    this.image = "images/weapons/tickle.jpeg";
+   this.sound = "sound/weapons/ticklefingers.mp3";
   };
   oldGauntlet.Weapons.TickleFingers.prototype = new oldGauntlet.Weapons.Weapon();
 
@@ -86,6 +95,7 @@ var Gauntlet = (function(oldGauntlet) {
    this.damage = 27;
    this.hands = 4;
    this.image = "images/weapons/hadooken.png";
+   this.sound = "sound/weapons/hadooken.mp3";
   };
   oldGauntlet.Weapons.Hadooken.prototype = new oldGauntlet.Weapons.Weapon();
 
@@ -94,6 +104,7 @@ var Gauntlet = (function(oldGauntlet) {
       this.damage = 8;
       this.hands = 3;
       this.image = "images/weapons/leafshield.jpg";
+      this.sound = "sound/weapons/wind.mp3";
   };
   oldGauntlet.Weapons.LeafShield.prototype = new oldGauntlet.Weapons.Weapon();
 
@@ -102,6 +113,7 @@ var Gauntlet = (function(oldGauntlet) {
       this.damage = 9;
       this.hands = 4;
       this.image = "images/weapons/atomicfire.png";
+      this.sound = "sound/weapons/atomicfire.mp3";
   };
   oldGauntlet.Weapons.AtomicFire.prototype = new oldGauntlet.Weapons.Weapon();
 
@@ -111,6 +123,7 @@ var Gauntlet = (function(oldGauntlet) {
       this.damage = 7;
       this.hands = 2;
       this.image = "images/weapons/scissor.jpg";
+      this.sound = "sound/weapons/scissors.wav";
   };
   oldGauntlet.Weapons.Scissors.prototype = new oldGauntlet.Weapons.Weapon();
 
